6b0ea612252b380f8ad0c634af6dc4ebce397208
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-09-07 20:47:13 +0600

    Project Scaffolding with Vite, React, and ShadCN

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+    
+      <h1>This is a simple react app with typescript and shadcn</h1>
+      <Button>Click me</Button>
+     
+    </>
+  )
+}
+
+export default App
