e8e39d2506a551dbad57ee45a6487c6f42238dc4
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-09-08 23:51:40 +0600

    Refining Spacing and Alignment for Visual Consistency

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,8 @@
 export default function About() {
   return (
-    <div>
-      About Component
+    <div className="container mx-auto px-8">
+      <h1>About us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptatem magni at unde eius rerum, quod molestias. Asperiores excepturi at ea minima quam facere dolorem magni fugit voluptatum. Tempora laboriosam quod consequuntur iusto eius nihil, eum voluptatem quae aperiam voluptates? Quia, ipsum labore deserunt dicta quisquam doloribus praesentium ab dignissimos cumque pariatur quos eos quod exercitationem iure omnis cupiditate alias quo eaque natus blanditiis?</p>
     </div>
   );
 }
