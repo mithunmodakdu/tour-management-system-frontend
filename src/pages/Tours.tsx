92523d6b5b0f45f4d3b442a9772aec15d9f2a339
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-10-14 23:49:38 +0600

    working on searchParams for TourFilters

--- src/pages/Tours.tsx
+++ src/pages/Tours.tsx
@@ -1,108 +1,23 @@
+import TourFilters from "@/components/modules/Tours/TourFilters";
 import { Button } from "@/components/ui/button";
 import {
   useGetAllToursQuery,
-  useGetTourTypeQuery,
 } from "@/redux/features/tour/tour.api";
 import { Link } from "react-router";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { useGetDivisionsQuery } from "@/redux/features/division/division.api";
-import { useState } from "react";
+
 
 export default function Tours() {
-  const [selectedDivision, setSelectedDivision] = useState<string | undefined>(undefined);
-  const [selectedTourType, setSelectedTourType] = useState<string | undefined>(undefined);
+  
   // console.log(selectedDivision)
 
-  const { data } = useGetAllToursQuery({division: selectedDivision, tourType: selectedTourType});
+  const { data } = useGetAllToursQuery(undefined);
   // console.log(data);
 
-  const { data: divisionData, isLoading: divisionLoading } = useGetDivisionsQuery(undefined);
-  const { data: tourTypeData, isLoading: tourTypeLoading } = useGetTourTypeQuery(undefined);
-
-  const divisionOptions = divisionData?.map(
-    (item: { name: string; _id: string }) => ({
-      divisionName: item.name,
-      divisionId: item._id,
-    })
-  );
   
-  const tourTypeOptions = tourTypeData?.map(
-    (item: { name: string; _id: string }) => ({
-      tourTypeName: item.name,
-      tourTypeId: item._id,
-    })
-  );
-
-  const handleClearFilter = () =>{
-    setSelectedDivision(undefined);
-    setSelectedTourType(undefined)
-  }
 
   return (
     <div className="flex gap-10">
-      <div className="col-span-3 w-1/4 h-[500px] border border-muted rounded-md p-5 space-y-4">
-        <div className="flex justify-between items-center">
-          <h1>Filters</h1>
-          <Button onClick={handleClearFilter} size="sm" variant="outline">
-            Clear Filter
-          </Button>
-        </div>
-        <div>
-          <Label className="mb-2">Division to visit</Label>
-          <Select
-            onValueChange={(value) => setSelectedDivision(value)}
-            disabled= {divisionLoading}
-          >
-            <SelectTrigger className="w-full">
-              <SelectValue />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Divisions</SelectLabel>
-                {divisionOptions?.map(
-                  (item: { divisionName: string; divisionId: string }) => (
-                    <SelectItem key={item.divisionId} value={item.divisionId}>
-                      {item.divisionName}
-                    </SelectItem>
-                  )
-                )}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-        <div>
-          <Label className="mb-2">Tour Type</Label>
-          <Select
-            onValueChange={(value) => setSelectedTourType(value)}
-            disabled={tourTypeLoading}
-          >
-            <SelectTrigger className="w-full">
-              <SelectValue />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Tour Type</SelectLabel>
-                {tourTypeOptions?.map(
-                  (item: { tourTypeId: string; tourTypeName: string }) => (
-                    <SelectItem key={item.tourTypeId} value={item.tourTypeId}>
-                      {item.tourTypeName}
-                    </SelectItem>
-                  )
-                )}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
+      <TourFilters/>
       <div className="container mx-auto px-5 py-8 grid grid-cols-12 gap-5">
         <div className="col-span-9 w-full">
           {data?.map((item) => (
