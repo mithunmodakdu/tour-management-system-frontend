d6961c9b71c1a648dde0db2fe5a0ca966d1085b2
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-10-19 17:04:25 +0600

    EditDivisionModal, divisionGetBySlug endpoint created

--- src/pages/Admin/AddDivision.tsx
+++ src/pages/Admin/AddDivision.tsx
@@ -1,5 +1,6 @@
 import { DeleteConfirmation } from "@/components/DeleteConfirmation";
 import { AddDivisionModal } from "@/components/modules/Admin/Division/AddDivisionModal";
+import { EditDivisionModal } from "@/components/modules/Admin/Division/EditDivisionModal";
 import { Button } from "@/components/ui/button";
 import {
   Table,
@@ -9,44 +10,54 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useGetDivisionsQuery, useRemoveDivisionMutation } from "@/redux/features/division/division.api";
+import {
+  useGetDivisionsQuery,
+  useRemoveDivisionMutation,
+} from "@/redux/features/division/division.api";
 import { Trash2 } from "lucide-react";
 import { toast } from "sonner";
 
 export default function AddDivision() {
-  const {data} = useGetDivisionsQuery(undefined);
+  const { data } = useGetDivisionsQuery(undefined);
   const [removeDivision] = useRemoveDivisionMutation();
 
   const handleRemoveDivision = async (divisionId: string) => {
-    const toastId = toast.loading("Removing Division...")
+    const toastId = toast.loading("Removing Division...");
     try {
       const res = await removeDivision(divisionId).unwrap();
       if (res.success) {
-        toast.success("Division removed successfully.", {id: toastId});
+        toast.success("Division removed successfully.", { id: toastId });
       }
     } catch (error) {
       console.error(error);
     }
   };
 
+
   return (
     <div className="w-full max-w-xl mx-auto px-5">
       <div className="flex justify-between my-10">
         <h1 className="text-2xl font-semibold">Divisions</h1>
-        <AddDivisionModal/>
+        <AddDivisionModal />
       </div>
       <div className="border border-muted rounded-md">
         <Table>
           <TableHeader>
             <TableRow>
               <TableHead>Name</TableHead>
-              <TableHead>Action</TableHead>
+              <TableHead>Edit</TableHead>
+              <TableHead>Delete</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {data?.map((item: { _id: string; name: string }) => (
+            {data?.map((item: { _id: string; name: string; slug: string; }) => (
               <TableRow>
                 <TableCell>{item?.name}</TableCell>
+
+                <TableCell>
+                  <EditDivisionModal slug={item.slug}/>
+                </TableCell>
+
                 <TableCell>
                   <DeleteConfirmation
                     onConfirm={() => handleRemoveDivision(item._id)}
