2e89eb92122980891b0e97a4199c67c498f984ef
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-10-20 23:30:05 +0600

    TourDestinations component created

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/modules/homePage/HeroSection";
+import TourDestinations from "@/components/modules/homePage/TourDestinations";
 
 export default function HomePage() {
   return (
     <div>
       <HeroSection/>
+      <TourDestinations/>
     </div>
   );
 }
